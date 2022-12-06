c668128707e76c135abde0f618004a094d72b3af
Duc Nguyen <duc.nguyen@metub.net>
2022-12-07 03:38:25 +0700

    remove tsc, add deps

--- android/src/main/java/cn/jystudio/bluetooth/RNBluetoothEscposPrinterPackage.java
+++ android/src/main/java/cn/jystudio/bluetooth/RNBluetoothEscposPrinterPackage.java
@@ -6,7 +6,6 @@ import java.util.Collections;
 import java.util.List;
 
 import cn.jystudio.bluetooth.escpos.RNBluetoothEscposPrinterModule;
-import cn.jystudio.bluetooth.tsc.RNBluetoothTscPrinterModule;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -18,8 +17,7 @@ public class RNBluetoothEscposPrinterPackage implements ReactPackage {
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         BluetoothService service = new BluetoothService(reactContext);
         return Arrays.<NativeModule>asList(new RNBluetoothManagerModule(reactContext, service),
-                new RNBluetoothEscposPrinterModule(reactContext, service),
-                new RNBluetoothTscPrinterModule(reactContext, service));
+                new RNBluetoothEscposPrinterModule(reactContext, service));
     }
 
     // Deprecated from RN 0.47
